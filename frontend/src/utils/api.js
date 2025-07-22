5af9587f587cc5de212653de330418163f3b3856
aayush <agrawalaayu7862gmail.com>
2025-07-22 23:06:37 +0530

    fixed api

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,12 +1,32 @@
+// import axios from "axios";
+
+// const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5001/api";
+
+// const api = axios.create({
+//   baseURL: API_URL,
+// });
+
+// // Add token to requests
+// api.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("token");
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
+
+// export default api;
+
+
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5001/api";
+// Always use deployed backend URL
+const API_URL = "https://bharati-news.onrender.com/api";
 
 const api = axios.create({
   baseURL: API_URL,
 });
 
-// Add token to requests
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
